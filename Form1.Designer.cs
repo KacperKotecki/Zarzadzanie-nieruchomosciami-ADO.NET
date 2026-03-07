64ebdb73afd4c22d3b58d9d3f9cc33d17c872a68
Kacper Kotecki <kacperkotecki@o2.pl>
2026-03-07 17:48:46 +0100

    Dodanie nowej biblioteki Proste polaczenie z baza danych

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btn_Wczytaj_Dane = new System.Windows.Forms.Button();
+            this.btn_Dodaj = new System.Windows.Forms.Button();
+            this.btn_Usun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(1438, 735);
+            this.dgv.TabIndex = 0;
+            // 
+            // btn_Wczytaj_Dane
+            // 
+            this.btn_Wczytaj_Dane.Location = new System.Drawing.Point(12, 753);
+            this.btn_Wczytaj_Dane.Name = "btn_Wczytaj_Dane";
+            this.btn_Wczytaj_Dane.Size = new System.Drawing.Size(125, 44);
+            this.btn_Wczytaj_Dane.TabIndex = 1;
+            this.btn_Wczytaj_Dane.Text = "Wczytaj Dane";
+            this.btn_Wczytaj_Dane.UseVisualStyleBackColor = true;
+            this.btn_Wczytaj_Dane.Click += new System.EventHandler(this.btn_Wczytaj_Dane_Click);
+            // 
+            // btn_Dodaj
+            // 
+            this.btn_Dodaj.Location = new System.Drawing.Point(143, 753);
+            this.btn_Dodaj.Name = "btn_Dodaj";
+            this.btn_Dodaj.Size = new System.Drawing.Size(125, 44);
+            this.btn_Dodaj.TabIndex = 2;
+            this.btn_Dodaj.Text = "Dodaj";
+            this.btn_Dodaj.UseVisualStyleBackColor = true;
+            this.btn_Dodaj.Click += new System.EventHandler(this.btn_Dodaj_Click);
+            // 
+            // btn_Usun
+            // 
+            this.btn_Usun.Location = new System.Drawing.Point(274, 753);
+            this.btn_Usun.Name = "btn_Usun";
+            this.btn_Usun.Size = new System.Drawing.Size(125, 44);
+            this.btn_Usun.TabIndex = 3;
+            this.btn_Usun.Text = "Usun";
+            this.btn_Usun.UseVisualStyleBackColor = true;
+            this.btn_Usun.Click += new System.EventHandler(this.btn_Usun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1678, 823);
+            this.Controls.Add(this.btn_Usun);
+            this.Controls.Add(this.btn_Dodaj);
+            this.Controls.Add(this.btn_Wczytaj_Dane);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btn_Wczytaj_Dane;
+        private System.Windows.Forms.Button btn_Dodaj;
+        private System.Windows.Forms.Button btn_Usun;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,47 @@ namespace Zarzadzanie_nieruchomosciami_ADO.NET
 {
     public partial class Form1 : Form
     {
+        private string connectionString = @"Provider=SQLOLEDB;Data Source=PC\SQLEXPRESS;Initial Catalog=ZarzadzanieNieruchomosciami;Integrated Security=SSPI;";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_Wczytaj_Dane_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Wlasciciele";
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    
+                    OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+
+                    
+                    DataTable dataTable = new DataTable();
+
+                    
+                    adapter.Fill(dataTable);
+
+                    
+                    dgv.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                { 
+                    MessageBox.Show("Nie udało się pobrać danych: " + ex.Message);
+                }
+            }
+        }
+
+        private void btn_Dodaj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Usun_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
